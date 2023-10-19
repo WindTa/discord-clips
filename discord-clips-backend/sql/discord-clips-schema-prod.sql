2f190dc3cfaabe4fbe4a8b91582bc5c811247de2
WindTa <john.k.tata@gmail.com>
2023-10-19 16:59:39 -0700

    Updated sql files with discord_server

--- discord-clips-backend/sql/discord-clips-schema-prod.sql
+++ discord-clips-backend/sql/discord-clips-schema-prod.sql
@@ -1,9 +1,21 @@
-drop database if exists discord_clips;
-create database discord_clips;
-use discord_clips;
+drop database if exists discord_clips_test;
+create database discord_clips_test;
+use discord_clips_test;
 
 -- create tables and relationships
 create table discord_user (
 	discord_user_id bigint primary key,
     username varchar(32) not null
 );
+
+create table discord_server (
+    discord_server_id bigint primary key,
+    servername varchar(32) not null
+);
+
+-- insert default values
+insert into discord_user (discord_user_id, username) values
+	(221863292681977857, 'windta');
+
+insert into discord_server (discord_server_id, servername) values
+    (1161381438839607358, "WindTa's server");
--- discord-clips-backend/sql/discord-clips-schema-test.sql
+++ discord-clips-backend/sql/discord-clips-schema-test.sql
@@ -9,22 +9,30 @@ create table discord_user (
 );
 
 create table discord_server (
+    discord_server_id bigint primary key,
+    servername varchar(32) not null
 );
 
 -- insert default values
 insert into discord_user (discord_user_id, username) values
 	(221863292681977857, 'windta');
+
+insert into discord_server (discord_server_id, servername) values
+    (1161381438839607358, "WindTa's server");
     
 -- set known good state
 delimiter //
 create procedure set_known_good_state()
 begin
+
 	delete from discord_user;
+    delete from discord_server;
+
+    insert into discord_user (discord_user_id, username) values
+        (221863292681977857, 'windta');
+
+    insert into discord_server (discord_server_id, servername) values
+        (1161381438839607358, "WindTa's server");
     
-    insert into discord_user
-		(discord_user_id, username) 
-	values
-		(221863292681977857, 'windta');
-	
 end //
 delimiter ;
