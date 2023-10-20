927eb7747dc5f9a580fcf9ff9d99273c0eff178f
WindTa <john.k.tata@gmail.com>
2023-10-20 11:15:09 -0700

    Removed supabase. Updated navbar

--- discord-clips-frontend/src/App.js
+++ discord-clips-frontend/src/App.js
@@ -1,65 +1,23 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
 import Home from './pages/Home';
 import NavBar from './components/NavBar/NavBar';
-import UserContext from './context/UserContext';
-
-import supabaseClient from './services/SupabaseClient';
 
 function App() {
-    const [user, setUser] = useState(null);
-    const [session, setSession] = useState(null);
-
-    useEffect(() => {
-        async function getUserData() {
-            await supabaseClient.auth.getUser().then((value) => {
-                if (value.data?.user) {
-                    setUser(value);
-                }
-            })
-        }
-        getUserData();
-    }, [])
-
-    useEffect(() => {
-        async function getSessionData() {
-            await supabaseClient.auth.getSession().then((value) => {
-                if (value.data?.session) {
-                    setSession(value);
-                }
-            })
-        }
-        getSessionData();
-    }, [])
-
-    async function signout() {
-        const { error } = await supabaseClient.auth.signOut();
-        setUser(null);
-    }
-
-    const auth = {
-        user: user,
-        session: session,
-        signout
-    }
-
-    console.log(user);
-
     return (
-        <UserContext.Provider value={auth}>
-            <Router>
-                <header className='my-1'>
-                    <NavBar />
-                </header>
-                <main className='container'>
-                    <Routes>
-                        <Route path="/" element={<Home />}/>
-                    </Routes>
-                </main>
-            </Router>
-        </UserContext.Provider>
+        <Router>
+            <header className='my-1'>
+                <NavBar />
+            </header>
+            <main className='container'>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/menu" element={<div>Menu</div>}/>
+                    <Route path="/signin" element={<div>Sign In</div>}/>
+                </Routes>
+            </main>
+        </Router>
     );
 }
 
--- discord-clips-frontend/src/services/SupabaseClient.js
+++
@@ -1,6 +0,0 @@
-import config from '../config.json';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseClient = createClient(config.supabaseUrl, config.supabaseKey);
-
-export default supabaseClient;
