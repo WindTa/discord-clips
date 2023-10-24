1dee90e8c3595ec56de7f20fed94124a081eda8f
WindTa <john.k.tata@gmail.com>
2023-10-23 19:46:34 -0700

    Cleaned up extra files

--- discord-clips-frontend/src/index.js
+++ discord-clips-frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './contexts/AuthProvider';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -19,8 +17,3 @@ root.render(
         </Router>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
