0fb43be5cf1c645e9c949b8eaf89b34efc8ed898
WindTa <john.k.tata@gmail.com>
2023-10-25 21:02:35 -0700

    react player now responds to replay and start and duration changes

--- discord-clips-frontend/src/components/clips/ClipEmbed.js
+++ discord-clips-frontend/src/components/clips/ClipEmbed.js
@@ -1,40 +1,73 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
 
 import Ratio from 'react-bootstrap/Ratio';
 import ReactPlayer from 'react-player';
 import MultiRangeSlider from 'multi-range-slider-react';
 
 function ClipEmbed({clip}) {
-    const [start, setStart] = useState(clip ? clip.startTime : 0);
-    const [end, setEnd] = useState(clip ? (clip.startTime + clip.duration) : 5);
-    const [duration, setDuration] = useState(0);
+    const playerRef = useRef();
+    const [timer, setTimer] = useState(null);
+    const [playing, setPlaying] = useState(false);
 
-    const handleSliderInput = (e) => {
+    const [start, setStart] = useState(clip ? clip.startTime : 0);
+    const [duration, setDuration] = useState(clip ? clip.duration : 5);
+    const [volume, setVolume] = useState(clip ? clip.volume : 1);
+    const [videoLength, setVideoLength] = useState(0);
+
+    // Should replay the video any time there are value changes
+    useEffect(() => {
+        replay();
+    }, [start, duration, volume])
+
+    // Update clip values
+    const handleInput = (e) => {
         setStart(e.minValue);
-        setEnd(e.maxValue);
+        setDuration(e.maxValue - e.minValue);
     }
 
-    const handleEmbedDuration = (duration) => {
-        setDuration(duration);
+    const handleVolume = (e) => {
+        console.log(e.volume);
+        setVolume(e.volume);
+    }
+
+    // Handle range slider end value
+    const handleVideoLength = (videoLength) => {
+        setVideoLength(videoLength);
+    }
+
+    // Function to replay clip
+    const replay = () => {
+        clearTimeout(timer);
+        playerRef.current.seekTo(start, 'seconds');
+        setPlaying(true);
+        const newTimer = setTimeout(() => {
+            setPlaying(false);
+        }, duration * 1000);
+
+        setTimer(newTimer);
     }
 
     return (
         <div>
             <Ratio aspectRatio="16x9">
                 <ReactPlayer 
+                    ref={playerRef}
                     className='h-100 w-100' 
+                    controls="true"
+                    playing={playing}
                     url={`https://www.youtube.com/watch?v=${clip?.youtubeId}`} 
-                    onDuration={handleEmbedDuration}
+                    onDuration={handleVideoLength}
+                    onVolumeChange={handleVolume}
                 />
             </Ratio>
-            <MultiRangeSlider 
-                min={0} max={duration} step={0.1}
-                minValue={start} maxValue={end}
-                ruler='false' barInnerColor='red' 
-                style={{border: 'none', boxShadow: 'none', padding: '0px'}}
-                onInput={handleSliderInput}
+            <MultiRangeSlider ruler='false' barInnerColor='red' style={{border: 'none', boxShadow: 'none', padding: '0px'}}
+                min={0} max={videoLength} step={0.1}
+                minValue={start} maxValue={start + duration}
+                onInput={handleInput}
             />
+            <button onClick={replay}>
+                <h1>Replay</h1>
+            </button>
         </div>
     );
 }
